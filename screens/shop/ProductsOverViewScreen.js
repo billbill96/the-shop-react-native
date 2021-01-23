559457a07484762ff61fa488c9799116574b4901
Supannee Mutitanon <billbillgm@gmail.com>
2021-01-23 14:28:55 +0700

    implement cart screen

--- screens/shop/ProductsOverViewScreen.js
+++ screens/shop/ProductsOverViewScreen.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { FlatList } from 'react-native';
+import { FlatList, Platform } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
+import HeaderButton from '../../components/UI/HeaderButton';
 import ProductIem from '../../components/shop/ProductItem';
 import * as cartActions from '../../store/actions/cart';
 
@@ -29,8 +31,17 @@ const ProductsOverviewScreen = props => {
             />
 };
 
-ProductsOverviewScreen.navigationOptions = {
-    headerTitle: 'All Product'
+ProductsOverviewScreen.navigationOptions = navData => {
+    return {
+        headerTitle: 'All Product',
+        headerRight: <HeaderButtons HeaderButtonComponent={HeaderButton}>
+            <Item title='Cart' 
+                iconName={Platform.OS === 'android' ? 'md-cart' : 'ios-cart'} 
+                onPress={() => {
+                    navData.navigation.navigate('Cart')
+                }}/>
+        </HeaderButtons>
+    };
 };
 
 export default ProductsOverviewScreen;
