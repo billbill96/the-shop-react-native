4cc6b1411cbafe7d3179d1b8c8a9356ea4dfe203
Supannee Mutitanon <billbillgm@gmail.com>
2021-02-20 16:18:11 +0700

    text input config

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -40,7 +40,14 @@ const EditProductScreen = props => {
                     <TextInput 
                         style={styles.input} 
                         value={title} 
-                        onChangeText={text => setTitle(text)}/>
+                        onChangeText={text => setTitle(text)}
+                        keyboardType='default'
+                        autoCapitalize='sentences'
+                        autoCorrect
+                        returnKeyType='next'
+                        onEndEditing={() => console.log('onEditing')}
+                        onSubmitEditing={() => console.log('onSummitEditing')}
+                    />
                 </View>
                 <View style={styles.formControl}>
                     <Text style={styles.label}>Image URL</Text>
@@ -54,7 +61,9 @@ const EditProductScreen = props => {
                     <TextInput 
                         style={styles.input} 
                         value={price} 
-                        onChangeText={text => setPrice(text)}/>
+                        onChangeText={text => setPrice(text)}
+                        keyboardType='decimal-pad'
+                        />
                 </View>)}
                 <View style={styles.formControl}>
                     <Text style={styles.label}>Description</Text>
